1897bcddebcacfa29e4bc3d97ee0272f950320aa
default <bthistle@huronperthcatholic.ca>
2025-09-29 13:52:41 +0000

    added a Go script Doc Comment

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
+/*
+* Author: Brayden Thistle
+* Version: 1.0.0
+* Date: 2025-09-29
+*/
+
 package main
 
 import "fmt"
 
 func main() {
+	// print out the greeting
 	fmt.Println("Hello, World")
 }
